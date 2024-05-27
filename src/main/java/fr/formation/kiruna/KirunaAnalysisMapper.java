025b5390af12c440898a55b239bc3263afb1118a
P LBL <pauline.loyer@gmail.com>
2024-05-27 09:34:26 +0200

    Mapper Kiruna

--- src/main/java/fr/formation/kiruna/KirunaAnalysisMapper.java
+++ src/main/java/fr/formation/kiruna/KirunaAnalysisMapper.java
@@ -3,28 +3,40 @@ package fr.formation.kiruna;
 import java.io.IOException;
 import java.math.BigDecimal;
 
-import org.apache.hadoop.io.IntWritable;
+import org.apache.commons.lang3.compare.ComparableUtils;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.io.Text;
 
-public class KirunaAnalysisMapper extends MapReduceBase implements Mapper<LongWritable,Text,Text,IntWritable>{
+public class KirunaAnalysisMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, DoubleWritable> {
 
     @Override
-    public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter)
+    public void map(LongWritable key, Text value, OutputCollector<Text, DoubleWritable> output, Reporter reporter)
             throws IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'map'");
+        if (key.get() == 0) {
+            return;
+        }
+
+        KirunaDataEntry data = this.parseKiruna(value.toString());
+        BigDecimal limiteHaute = new BigDecimal("9000");
+        BigDecimal limiteBasse = new BigDecimal("11000");
+        if(!(data.getX()==null) && ComparableUtils.is(data.getX()).between(limiteBasse,limiteHaute)){
+            output.collect(new Text(data.getDate()),new DoubleWritable(data.getX().doubleValue()));
+        }
+
     }
-    
-    public KirunaDataEntry parseKiruna(String data){
+
+    public KirunaDataEntry parseKiruna(String data) {
         String[] values = data.split(";");
         KirunaDataEntry kirunaData = new KirunaDataEntry();
         kirunaData.setDate(values[0]);
-        if(values[1].length() != 0){kirunaData.setX(new BigDecimal(values[1]));}
+        if (values[1].length() != 0) {
+            kirunaData.setX(new BigDecimal(values[1]));
+        }
         return kirunaData;
     }
 }
